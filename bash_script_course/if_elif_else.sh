0b93ecdef79aa700d3a01b28146eb3970792fdc7
“dimasoprimeiro <ale.cayres@gmail.com>
2026-05-03 10:54:30 -0300

    mais um komitassu namoral

---
+++ bash_script_course/if_elif_else.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+if [ "${1,,}" = dimas ]; then
+    echo "Oh, voce eh o vidaloka supremo!"
+elif [ "${1,,}" = help ]; then
+    echo "Entra seu usuario namora ai!"
+else
+    echo "Eu naum sei quem vc é, mas voce nao eh o vidaloka suprema!"
+fi
