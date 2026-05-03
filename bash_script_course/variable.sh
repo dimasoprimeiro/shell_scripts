4cbcbde7491c55e8334b8c8e9872127400088b82
“dimasoprimeiro <ale.cayres@gmail.com>
2026-05-03 12:09:25 -0300

    mais um komitassu namoral

--- bash_script_course/variable.sh
+++ bash_script_course/variable.sh
@@ -13,7 +13,3 @@ MY_LOCATION_TO=/my/location/to
 cp $MY_LOCATION_FROM $MY_LOCATION_TO
 cp "$MY_LOCATION_FROM/here" "$MY_LOCATION_TO/there"
 
-MY_FIRST_LIST=(one two three four five)
-echo $MY_FIRST_LIST
-echo ${MY_FIRST_LIST[@]} #imprime a lista toda
-echo ${MY_FIRST_LIST[0]} #imprime o primeiro valor
