aca29c0b0f41bbf147032675ce0379c796735e9a
“dimasoprimeiro <ale.cayres@gmail.com>
2026-05-03 15:58:44 -0300

    mais um komitassu namoral

--- bash_script_course/functionpostargu.sh
+++ bash_script_course/functionpostargu.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 showname(){
     echo hello "$1"
+    if [ "${1,,}" = dimas ]; then 
+        return 0
+    else
+        return 1
+    fi
 }
-showname Dimas
+showname "$1"
+
+if [ $? = 1 ]; then 
+    echo "Alguem fora do conhecimento da vidalokagem chamou essa função"
+fi
