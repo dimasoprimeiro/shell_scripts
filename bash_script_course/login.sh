7c09ee38ba12c226c1e83757893cf02f0864f818
“dimasoprimeiro <ale.cayres@gmail.com>
2026-05-03 11:53:28 -0300

    mais um komitassu namoral

---
+++ bash_script_course/login.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+case ${1,,} in 
+    dimas | xandao)
+        echo "Oi, voce eh o vidaloka aqui"
+        ;;
+    help)
+        echo "Só digite seu usuario ai nomaralzinha"
+        ;;
+    *)
+        echo "Oi, voce nao ef o vidaloka aqui, insira um usuario vidaloka"
+esac
