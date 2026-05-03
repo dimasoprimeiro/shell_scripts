c2bcdb261519c1aa1e9ebf51dfb1ae983a3631e2
“dimasoprimeiro <ale.cayres@gmail.com>
2026-05-03 08:22:01 -0300

    mais um komitassu namoral

---
+++ bash_script_course/hello_there.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+FIRST_NAME=Dimas
+LAST_NAME=O_Primeiro
+
+echo Hello $FIRST_NAME $LAST_NAME
---
+++ bash_script_course/interactive_shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo What is yout first name?
+read -r FIRST_NAME
+echo What is your last name?
+read -r LAST_NAME
+
+echo Hello "$FIRST_NAME" "$LAST_NAME"
